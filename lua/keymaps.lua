971334768354f05e7fac2a3cdf29c9ea8aebb851
yonagi <lecterkn@proton.me>
2025-09-17 07:38:03 +0900

    feat: vertical terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,10 +2,9 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>x', vim.cmd.bd)
-vim.keymap.set('n', '<leader>t', vim.cmd.terminal)
+vim.keymap.set('n', '<leader>t', ':vert belowright term<CR>')
 
-vim.keymap.set("v", "<leader>yy", '"+yy<CR>')
-
+vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>')
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
@@ -14,7 +13,7 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format);
 
 vim.keymap.set('n', '<leader>su', vim.pack.update)
-vim.keymap.set('n', '<leader>cu', 
+vim.keymap.set('n', '<leader>cu',
     function()
         local plugins = {}
         for _, plugin in ipairs(vim.pack.get()) do
