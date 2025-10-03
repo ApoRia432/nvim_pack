7003daa3dc6ed9bce4566ddf0920d65b9641392e
yonagi <lecterkn@proton.me>
2025-10-04 05:39:33 +0900

    feat: add indent

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,21 @@
 local plugins = {
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-  { src = 'https://github.com/stevearc/oil.nvim' },
-  { src = 'https://github.com/saghen/blink.cmp' },
-  { src = 'https://github.com/rafamadriz/friendly-snippets' },
-  { src = 'https://github.com/nvim-mini/mini.pick' },
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-  { src = 'https://github.com/folke/trouble.nvim' },
-  { src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim' },
-  -- 'https://github.com/akinsho/bufferline.nvim',
-  -- { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://github.com/stevearc/oil.nvim' },
+    { src = 'https://github.com/saghen/blink.cmp' },
+    { src = 'https://github.com/rafamadriz/friendly-snippets' },
+    { src = 'https://github.com/nvim-mini/mini.pick' },
+    { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+    { src = 'https://github.com/folke/trouble.nvim' },
+    { src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim' },
+    -- 'https://github.com/akinsho/bufferline.nvim',
+    { src = 'https://github.com/lukas-reineke/indent-blankline.nvim' },
 }
 
 vim.pack.add(plugins)
 -- require("plugins.bufferline")
--- require("plugins.indent")
+require("plugins.indent")
 require("plugins.oil")
 require("plugins.git")
 require("plugins.blink")
--- lua/plugins/pick.lua
+++ lua/plugins/pick.lua
@@ -2,5 +2,5 @@ require('mini.pick').setup()
 
 vim.keymap.set('n', '<leader>pf', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>pb', ':Pick buffers<CR>')
-vim.keymap.set('n', '<leader>pg', ":Pick files tool='git'<CR>")
-vim.keymap.set('n', '<leader>ps', ":Pick grep_live<CR>")
+vim.keymap.set('n', '<leader>pg', ':Pick files tool="git"<CR>')
+vim.keymap.set('n', '<leader>ps', ':Pick grep_live<CR>')
