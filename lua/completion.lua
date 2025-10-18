5930463fb31e8eb061724ac19aba2a7ce3a4cdd2
ApoRia432 <lecterkn@proton.me>
2025-10-18 16:57:13 +0900

    fix: completion

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,11 +1,13 @@
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            client.server_capabilities.completionProvider.triggerCharacters = chars
+        if client and client:supports_method('textDocument/completion') then
+            -- set trigger keys
+            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+            -- client.server_capabilities.completionProvider.triggerCharacters = chars
+            -- enable completion
             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
         end
     end,
 })
-vim.cmd("set completeopt+=noselect")
+vim.opt.completeopt = { "menu", "menuone", "noinsert" }
